27ca358f7b9289e4aa336e63c0cdc80f4e9e12db
Kelsey <Kelsey@KELSEYCROMPA79B>
2022-02-06 19:09:37 -0500

    Add project files.

---
+++ HrPtoManagement.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HrPtoManagement.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
